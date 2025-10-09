2351e58fd702d76e857d81e1aeebf294d4bfb839
Sumit1642 <sumeetyadav1642@gmail.com>
2025-10-09 11:43:52 +0530

    Added tiptap table , heading, bullets

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { TaskItem, TaskList } from "@tiptap/extension-list";
+import { Table, TableCell, TableHeader, TableRow } from "@tiptap/extension-table";
 
 export const Editor = () => {
 	const editor = useEditor({
@@ -11,7 +13,17 @@ export const Editor = () => {
 				class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
 			},
 		},
-		extensions: [StarterKit],
+		extensions: [
+			StarterKit,
+			TaskItem.configure({
+				nested: true,
+			}),
+			TaskList,
+			Table,
+			TableCell,
+			TableHeader,
+			TableRow
+		],
 		content: "<p>Hello World! 🌎️</p>",
 		// Don't render immediately on the server to avoid SSR issues
 		immediatelyRender: false,
